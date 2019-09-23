2b801de74b40be6b69d4a9ac4d1537856ad2a909
Kevin Park <shp291800@gmail.com>
2019-09-22 21:08:19 -0700

    portfolio

--- static/portfolio.js
+++ static/portfolio.js
@@ -1,4 +1,4 @@
-<script>
+
 $(document).ready(function(){
   
   $("a").on('click', function(event) {
@@ -23,4 +23,48 @@ $(document).ready(function(){
   })
 
 });
-</script>
+
+
+jQuery(document).ready(function() {
+  
+  var btn = $('#button');
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
+
+});
+
+var btn = $('#button');
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
+
+
+
+
+
+
+
+
+
+
